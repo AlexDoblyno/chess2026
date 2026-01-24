e0d6bf12f27a22e222f96276fa44adc5fe0d9481
AlexDoblyno <alexdoblyno@gmail.com>
2026-01-23 20:59:57 -0700

    完成chessboard

--- shared/src/main/java/chess/ChessMove.java
+++ shared/src/main/java/chess/ChessMove.java
@@ -1,5 +1,7 @@
 package chess;
 
+import java.util.Objects;
+
 /**
  * Represents moving a chess piece on a chessboard
  * <p>
@@ -8,22 +10,40 @@ package chess;
  */
 public class ChessMove {
 
-    public ChessMove(ChessPosition startPosition, ChessPosition endPosition,
+    private ChessPosition StartPosition;
+    private ChessPosition EndPosition;
+    private ChessPiece.PieceType promotionPiece;
+
+    public ChessMove(ChessPosition StartPosition, ChessPosition EndPosition,
                      ChessPiece.PieceType promotionPiece) {
+        this.StartPosition = StartPosition;
+        this.EndPosition = EndPosition;
+        this. promotionPiece = promotionPiece;
+    }
+
+    /** New constructor with null promotionpiece object
+     *
+     * @param StartPosition what it looks like
+     * @param EndPosition what it sounds like
+     */
+    public ChessMove(ChessPosition StartPosition, ChessPosition EndPosition) {
+        this.StartPosition = StartPosition;
+        this.EndPosition = EndPosition;
+        this. promotionPiece = null;
     }
 
     /**
      * @return ChessPosition of starting location
      */
     public ChessPosition getStartPosition() {
-        throw new RuntimeException("Not implemented");
+        return StartPosition;
     }
 
     /**
      * @return ChessPosition of ending location
      */
     public ChessPosition getEndPosition() {
-        throw new RuntimeException("Not implemented");
+        return EndPosition;
     }
 
     /**
@@ -33,6 +53,22 @@ public class ChessMove {
      * @return Type of piece to promote a pawn to, or null if no promotion
      */
     public ChessPiece.PieceType getPromotionPiece() {
-        throw new RuntimeException("Not implemented");
+        return promotionPiece;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof ChessMove chessMove)) {
+            return false;
+        }
+        return Objects.equals(StartPosition, chessMove.StartPosition) && Objects.equals(EndPosition, chessMove.EndPosition) && promotionPiece == chessMove.promotionPiece;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(StartPosition, EndPosition, promotionPiece);
     }
 }
