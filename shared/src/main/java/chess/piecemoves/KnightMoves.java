db845f6f1f72ab9c0a03d94011b8651963eac36a
AlexDoblyno <alexdoblyno@gmail.com>
2026-01-23 21:08:16 -0700

    完成chessmoves

--- shared/src/main/java/chess/piecemoves/KnightMoves.java
+++ shared/src/main/java/chess/piecemoves/KnightMoves.java
@@ -1,4 +1,25 @@
 package chess.piecemoves;
 
-public class KnightMoves {
+import chess.ChessBoard;
+import chess.ChessPiece;
+import chess.ChessPosition;
+
+public class KnightMoves extends PieceMoves {
+    public KnightMoves(ChessBoard GameBoard, ChessPosition StartPosition) {
+        super(GameBoard, StartPosition);
+        calculateMoves();
+    }
+
+    @Override
+    public void calculateMoves() {
+        ChessPosition checkPosition;
+        for (int row = StartPosition.getRow() - 2; row <= StartPosition.getRow() + 2; row++) {
+            for (int col = StartPosition.getColumn() - 2; col <= StartPosition.getColumn() + 2; col++) {
+                if (Math.abs(StartPosition.getRow() - row) + Math.abs(StartPosition.getColumn() - col) == 3 && isInBounds(row, col)) {
+                    checkPosition = new ChessPosition(row, col);
+                    checkSpace(checkPosition);
+                }
+            }
+        }
+    }
 }
