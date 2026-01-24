b9fe19f55ed68d1d4085961c4c79584395a3e7b4
AlexDoblyno <alexdoblyno@gmail.com>
2026-01-23 21:00:22 -0700

    完成chessboard

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,6 +1,10 @@
 package chess;
 
+import chess.piecemoves.*;
+
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Objects;
 
 /**
  * Represents a single chess piece
@@ -10,7 +14,14 @@ import java.util.Collection;
  */
 public class ChessPiece {
 
+    private ChessGame.TeamColor pieceColor;
+    private PieceType type;
+    private PieceMoves selectedPiece;
+    private HashSet<PieceMoves> moveList;
+
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
+        this.pieceColor = pieceColor;
+        this.type = type;
     }
 
     /**
@@ -29,14 +40,14 @@ public class ChessPiece {
      * @return Which team this chess piece belongs to
      */
     public ChessGame.TeamColor getTeamColor() {
-        throw new RuntimeException("Not implemented");
+        return pieceColor;
     }
 
     /**
      * @return which type of chess piece this piece is
      */
     public PieceType getPieceType() {
-        throw new RuntimeException("Not implemented");
+        return type;
     }
 
     /**
@@ -47,6 +58,48 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        throw new RuntimeException("Not implemented");
+        return switch (board.getPiece(myPosition).getPieceType()) {
+            case ROOK -> {
+                selectedPiece = new RookMoves(board, myPosition);
+                yield selectedPiece.getMoveList();
+            }
+            case BISHOP -> {
+                selectedPiece = new BishopMoves(board, myPosition);
+                yield selectedPiece.getMoveList();
+            }
+            case QUEEN -> {
+                selectedPiece = new QueenMoves(board, myPosition);
+                yield selectedPiece.getMoveList();
+            }
+            case KNIGHT -> {
+                selectedPiece = new KnightMoves(board, myPosition);
+                yield selectedPiece.getMoveList();
+            }
+            case KING -> {
+                selectedPiece = new KingMoves(board, myPosition);
+                yield selectedPiece.getMoveList();
+            }
+            case PAWN -> {
+                selectedPiece = new PawnMoves(board, myPosition);
+                yield selectedPiece.getMoveList();
+            }
+            case null, default -> null;
+        };
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof ChessPiece that)) {
+            return false;
+        }
+        return pieceColor == that.pieceColor && type == that.type && Objects.equals(moveList, that.moveList);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pieceColor, type, moveList);
     }
 }
