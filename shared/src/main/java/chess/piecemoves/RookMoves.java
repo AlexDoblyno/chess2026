f873c4b78cdecc33e6543c9d141df7028399e5c8
AlexDoblyno <alexdoblyno@gmail.com>
2026-01-23 21:09:33 -0700

    完成chessmoves

--- shared/src/main/java/chess/piecemoves/RookMoves.java
+++ shared/src/main/java/chess/piecemoves/RookMoves.java
@@ -1,4 +1,13 @@
 package chess.piecemoves;
 
-public class RookMoves {
-}
+import chess.ChessBoard;
+import chess.ChessMove;
+import chess.ChessPosition;
+
+import java.util.HashSet;
+
+public class RookMoves extends PieceMovesFar {
+    public RookMoves(ChessBoard GameBoard, ChessPosition StartPosition) {
+        super(GameBoard, StartPosition);
+        calculateMoves();
+    }
