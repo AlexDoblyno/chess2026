c712c0b77ff063e1579ff54e7844e9b90143eb1f
AlexDoblyno <alexdoblyno@gmail.com>
2026-03-25 12:19:34 -0600

    Phase 5 第一次完全复制

---
+++ client/src/main/java/ui/GameUI.java
@@ -0,0 +1,47 @@
+package ui;
+
+import client.ChessClient;
+import exception.ResponseException;
+import exception.UIStateException;
+
+public class GameUI extends BaseUI {
+    private static Boolean isPlayer;
+    private static ChessboardDrawer drawer;
+
+    public GameUI(ChessClient client, ChessboardDrawer drawer, boolean isPlayer) {
+        super(client);
+        state = UIStatesEnum.GAMEUI;
+        GameUI.drawer = drawer;
+        GameUI.isPlayer = isPlayer;
+    }
+
+    @Override
+    public String handler(String input) throws ResponseException {
+        String[] tokens = input.split(" ");
+        switch(tokens[0].toLowerCase()) {
+            case "quit" -> quitGame();
+            default -> displayHelpInfo();
+        };
+        return null;
+    }
+
+    private void quitGame() throws ResponseException {
+        int gameID = client.getDataCache().getCurrentGameID()+1;
+        String gameName = client.getDataCache().getGameByIndex(gameID).gameName();
+
+        client.getDataCache().setCurrentGameID(0);
+        String returnStatement = "Left game " + gameName + "successfully.";
+        client.logout();
+        throw new UIStateException(new PostloginUI(client), returnStatement);
+    }
+
+    @Override
+    public String displayHelpInfo() {
+        return """
+    --- GAME COMMANDS ---
+    Type a command to get the corresponding action.
+    - quit      | Leave your current game.
+    - help      | Display this help menu.
+    """;
+    }
+}
