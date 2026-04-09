1b38c424d702813f996472bdf4d0265976ad62e3
AlexDoblyno <alexdoblyno@gmail.com>
2026-04-09 04:27:27 -0600

    Phase 6 从2025版本中添加shared的websocket commands and messages，添加test

---
+++ shared/src/main/java/websocket/commands/MakeMoveCommands.java
@@ -0,0 +1,16 @@
+package websocket.commands;
+
+import chess.ChessMove;
+
+public class MakeMoveCommands extends UserGameCommand {
+    private final ChessMove move;
+
+    public MakeMoveCommands(CommandType commandType, String authToken, Integer gameID, ChessMove move) {
+        super(commandType, authToken, gameID);
+        this.move = move;
+    }
+
+    public ChessMove getMove() {
+        return move;
+    }
+}
